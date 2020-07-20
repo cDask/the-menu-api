020adc791aa5da5e3ba19d6fcfd88a1ade95d1e1
Daniel Ask <cdanielask@gmail.com>
2020-07-20 15:31:53 +1000

    Feature: Added Ingredient model with validations and validation testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_051412) do
+ActiveRecord::Schema.define(version: 2020_07_20_052855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_07_20_051412) do
     t.string "name"
     t.string "info_type"
     t.string "info"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
