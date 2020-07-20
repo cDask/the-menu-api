b861db3a4f12f4aa95a3f3c300394325c5357a96
Daniel Ask <cdanielask@gmail.com>
2020-07-20 15:40:56 +1000

    Feature: Added Style model with validations and validation testing

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,3 +1,3 @@
 class Style < ApplicationRecord
-    validates :style_data, presence: true, format: { with: /{*}/, message: 'json only baby' }
+  validates :style_data, presence: true, format: { with: /{*}/, message: 'json only baby' }
 end
