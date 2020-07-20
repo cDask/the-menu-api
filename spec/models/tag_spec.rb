e7daa54d15137abb8416a1f67a2f7eb0a8f3c7e8
Daniel Ask <cdanielask@gmail.com>
2020-07-20 23:54:56 +1000

    Feature: Added association testing and associations to Tag model

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,18 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
+  let(:item_tags) { ItemTag.new }
+  let(:items) { Item.new }
   subject { described_class.new(
     name: "hoot hooot spicy"
   )}
   
   context 'validations' do 
-      it 'is valid with valid attributes' do
+    it 'is valid with valid attributes' do
       expect(subject).to be_valid
     end
 
     it 'is not valid without name' do
       subject.name = nil
       expect(subject).to_not be_valid
+    end
+  end
+
+  context 'associations' do
+    it 'has many item tags' do
+      relation = Tag.reflect_on_association(:item_tags)
+      expect(relation.macro).to eql(:has_many)
+    end
+
+    it 'has many items' do
+      relation = Tag.reflect_on_association(:items)
+      expect(relation.macro).to eql(:has_many)
     end
   end
 end
