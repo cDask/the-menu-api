79b606faa50a556d9b19e2823ce935ef68456584
Daniel Ask <cdanielask@gmail.com>
2020-07-21 20:13:55 +1000

    PM: Generated User, UserToken and Status controllers

---
+++ spec/requests/user_token_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "UserTokens", type: :request do
+    # describe 'GET #index' do
+    #   before(:example) do
+    #     @first_trail = create(:trail)
+    #     @last_trail = create(:trail)
+    #     get '/trails'
+    #     @json_response = JSON.parse(response.body)
+    #   end
+  
+    #   it 'returns http success' do
+    #     expect(response).to have_http_status(:success)
+    #   end
+  
+    #   it 'JSON response contains the correct number of entries' do
+    #     expect(@json_response['trails'].count).to eq(2)
+    #   end
+  
+    #   it 'JSON response body contains expected attributes' do
+    #     expect(@json_response['trails'][0]).to include({
+    #       'id' => @first_trail.id,
+    #       'name' => @first_trail.name,
+    #       'difficulty' => @first_trail.difficulty,
+    #     }) 
+    #   end
+    # end
+  end
+  
