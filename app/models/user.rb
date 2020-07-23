428a3d2cbb3694fea22b5f5bd921d8f15cd5446a
Daniel Ask <cdanielask@gmail.com>
2020-07-24 09:54:58 +1000

    FEATURE: Added email validation to test for uniqueness in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :full_name, presence: true
   has_many :restaurants
 end
