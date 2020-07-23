9dd8746d1851a4254534e204f925201d5c01ab79
Daniel Ask <cdanielask@gmail.com>
2020-07-23 12:37:39 +1000

    TESTING: Removed strong params testing and user token testing and fixed user sign up testing

--- spec/requests/user_token_request_spec.rb
+++ spec/requests/user_token_request_spec.rb
@@ -1,36 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "UserTokens", type: :request do
-    # describe 'POST #create' do
-    #     context 'when the user token is valid' do
-    #       before(:example) do
-    #         @user = create(:user)
-    #         post '/login', params: { auth: {email: @user.email, password: "password"} }
-    #       end
+    describe 'POST #create' do
+        context 'when the user token is valid' do
+          before(:example) do
+            @user = create(:user)
+            post '/login', params: { auth: {email: @user.email, password: "password"} }
+          end
     
-    #       it 'returns http created' do
-    #         expect(response).to have_http_status(:created)
-    #       end
-    #     end
-    
-    #     context 'when the user token is invalid' do
-    #       before(:example) do
-    #         post '/login', params: { auth: {email: ""}}
-    #         @json_response = JSON.parse(response.body)
-    #       end
-    
-    #       it 'returns status unprocessable entity' do
-    #         expect(response).to have_http_status(:not_found)
-    #       end
-    
-    #       it 'returns the correct number of errors' do
-    #         expect(@json_response['errors'].count).to eq(2)
-    #       end
-    
-    #       it 'errors contains the correct message' do
-    #         expect(@json_response['errors'][0]).to eq("Invalid Login")
-    #       end
-    #     end
-    #   end
+          it 'returns http created' do
+            expect(response).to have_http_status(:created)
+          end
+        end
+      end
   end
     
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -17,24 +17,24 @@ RSpec.describe "Users", type: :request do
           end
         end
     
-        # context 'when the user is invalid' do
-        #   before(:example) do
-        #     @user_params = attributes_for(:user, :invalid)
-        #     post '/sign-up', params: { user: @user_params }
-        #     @json_response = JSON.parse(response.body)
-        #   end
+        context 'when the user is invalid' do
+          before(:example) do
+            @user_params = attributes_for(:user, :invalid)
+            post '/sign-up', params: { user: @user_params }
+            @json_response = JSON.parse(response.body)
+          end
     
-        #   it 'returns status unprocessable entity' do
-        #     expect(response).to have_http_status(:unprocessable_entity)
-        #   end
+          it 'returns status unprocessable entity' do
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
     
-        #   it 'returns the correct number of errors' do
-        #     expect(@json_response['errors'].count).to eq(2)
-        #   end
+          it 'returns the correct number of errors' do
+            expect(@json_response['errors'].count).to eq(2)
+          end
     
-        #   it 'errors contains the correct message' do
-        #     expect(@json_response['errors'][0]).to eq("Name can't be blank")
-        #   end
-        # end
+          it 'errors contains the correct message' do
+            expect(@json_response['errors'][0]).to eq("Email can't be blank")
+          end
+        end
     end        
 end
