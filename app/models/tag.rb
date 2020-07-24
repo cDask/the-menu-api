2cf8dcd31deade9d23523b35ba9c40c8d1223ec4
Daniel Ask <cdanielask@gmail.com>
2020-07-24 10:00:52 +1000

    FEATURE: Added dependant destroy to all relevant relations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
-  has_many :item_tags
+  has_many :item_tags, dependent: :destroy
   has_many :items, through: :item_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :full_name, presence: true
-  has_many :restaurants
+  has_many :restaurants, dependent: :destroy
 end
