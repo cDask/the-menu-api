a64cfcc1911e87f04760935552e2344153eb1320
Daniel Ask <cdanielask@gmail.com>
2020-07-29 17:50:39 +1000

    PM: Added production url to cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080','https://the-menu-client.netlify.app
-    '
+    origins 'http://localhost:8080','https://the-menu-client.netlify.app'
 
     resource '*',
       headers: :any,
